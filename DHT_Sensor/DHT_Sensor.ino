a56179e029628ac202bd8146d547c59e6d21a20c
KrishMakhija <krishmakhija0603@gmail.com>
2026-01-07 13:07:34 +0530

    Add files via upload

---
+++ DHT_Sensor/DHT_Sensor.ino
@@ -0,0 +1,29 @@
+#include <DHT.h>
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  dht.begin();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  if(isnan(humidity) || isnan(temperature)){
+    Serial.println("Failed to read from DHT sensor!");
+    delay(2000);
+    return;
+  }
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print("%\t");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" ℃");
+  delay(2000);
+}
